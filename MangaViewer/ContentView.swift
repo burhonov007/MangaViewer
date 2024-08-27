741706fc9e37878262a8598972ab8af79afc31be
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2024-08-27 13:05:47 +0500

    Initial Commit

---
+++ MangaViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MangaViewer
+//
+//  Created by itserviceimac on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MangaViewer/MangaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaViewerApp.swift
+//  MangaViewer
+//
+//  Created by itserviceimac on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
